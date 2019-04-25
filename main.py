a0aac6e1d59d547ef51cd6e64ed14cd1f4fe3c0e
SurArt <48514218+SurArt@users.noreply.github.com>
2019-04-25 16:48:37 +0300

    Update main.py
    Добавил свой кусок алгоритма
    Кубиты отсчитываются сверху вниз как на картинке в телеграмме
    qr[0] - анцилла

--- main.py
+++ main.py
@@ -17,5 +17,36 @@ circuit.u3(0, 0, 0, qr[0])
 circuit.h(qr[0])
 circuit.cx(qr[0], qr[1])
 
+#...........................................
+# Third algorithm's step realisation
+
+
+# Decomposition of conditioned rotation:
+  
+def ConditionedRotation(angle, controllingQbit, subjectedQbit, circuit, quantReg):
+  
+    circuit.u3(angle, 0, 0, quantReg[subjectedQbit])
+    circuit.z(quantReg[subjectedQbit])
+    circuit.cx(quantReg[controllingQbit], quantReg[subjectedQbit])
+    circuit.u3( ( - angle ), 0, 0, quantReg[subjectedQbit])
+    circuit.z(quantReg[subjectedQbit])
+    
+    return
+
+
+#Execution in main coode:
+
+pi = 3.1416
+
+ConditionedRotation(pi/4, 1, 0, circuit, qr)
+ConditionedRotation(pi/8, 2, 0, circuit, qr)
+
+
+#...........................................
+
+
+
+
+
 # meas_qubits - кубиты, использующиеся для томографии. Порядок важен (для [0, 1] и [1, 0] ответ будет разным)
 print(launcher.run([circuit], meas_qubits=[0]))
